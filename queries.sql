3966811a314e5e71afa0279736404be221fde518
Amanda <75429795+chefpanda18@users.noreply.github.com>
2021-04-18 20:36:48 -0400

    Add files via upload

---
+++ queries.sql
@@ -0,0 +1,12 @@
+-- write your queries here
+
+SELECT * FROM owners o FULL JOIN vehicles v ON o.id=v.owner_id;
+
+SELECT first_name, last_name, COUNT (owner_id) FROM owners o JOIN vehicles v ON o.id=v.owner_id GROUP BY (first_name, last_name) ORDER BY first_name;    
+
+SELECT first_name, last_name, 
+    COUNT (owner_id), AVG (price) as average_price
+    FROM owners o JOIN vehicles v 
+    ON o.id=v.owner_id GROUP BY (first_name, last_name) 
+    HAVING COUNT(owner_id) > 1 AND AVG(price) > 10000
+    ORDER BY first_name DESC;
